63281969a5eabb5e06c6e1192e6a426e24cf749a
Grinch_ <user.grinch@gmail.com>
2022-05-21 18:03:52 +0600

    switch to windows-rs crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,32 @@
-use toy_arms;
+use std::thread;
 use std::os::raw::c_char;
+use windows::{
+    Win32::Foundation::*, Win32::System::SystemServices::*,
+};
 
-toy_arms::create_entrypoint!(main_thread);
-
-fn main_thread() {
-    loop {
-        if toy_arms::detect_keypress(toy_arms::VirtualKeyCode::VK_INSERT) {
-            unsafe {
-                // void __cdecl SetHelpMessage(const char*, bool, bool, bool)
-                type FHelpMSG = extern "cdecl" fn(*const c_char, c_char, c_char, c_char);
-                let func: FHelpMSG = std::mem::transmute(0x588BE0); 
-                func(b"Test Message\0".as_ptr() as *const c_char, 0, 0, 0);
-            }
+fn plugin_thread() 
+{
+    type FHelpMSG = extern "cdecl" fn(*const c_char, c_char, c_char, c_char);
+    loop 
+    {
+        unsafe 
+        {
+            // void __cdecl SetHelpMessage(const char*, bool, bool, bool)
+            let func: FHelpMSG = std::mem::transmute(0x588BE0); 
+            func(b"Test Message\0".as_ptr() as *const c_char, 0, 0, 0);
         }
+        thread::sleep(std::time::Duration::from_secs(1));
     }
 }
+
+#[no_mangle]
+extern "stdcall" fn DllMain( _ : HINSTANCE, fdw_reason: u32, _: u32) -> bool 
+{
+    if  fdw_reason == DLL_PROCESS_ATTACH 
+    {
+        thread::spawn(plugin_thread);
+    }
+
+    return true;  
+}
+
